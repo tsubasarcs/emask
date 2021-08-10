cf381852d28348b54731da8783373bb70f845b85
Sean Shih <tsubasarcs@gmail.com>
2021-08-11 03:52:41 +0800

    setup shop model, migration, and factory

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code',
+        'address',
+        'location',
+        'area',
+    ];
 }
--- database/factories/ShopFactory.php
+++ database/factories/ShopFactory.php
@@ -4,9 +4,19 @@ namespace Database\Factories;
 
 use App\Models\Shop;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class ShopFactory extends Factory
 {
+    protected array $address = [
+        '台北市松山區民權東路三段108號',
+        '台北市松山區民權東路三段160巷9號1樓',
+        '台北市松山區民權東路三段103巷15-1號',
+        '台北市松山區民權東路三段106巷11號',
+        '台北市松山區民權東路三段160巷11號',
+        '台北市松山區民權東路三段104號壹樓及地下室'
+    ];
+
     /**
      * The name of the factory's corresponding model.
      *
@@ -19,10 +29,11 @@ class ShopFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition(): array
     {
         return [
-            //
+            'code' => str_pad((string)rand(0, 999999999999999), 15, "0", STR_PAD_LEFT),
+            'address' => Arr::random($this->address),
         ];
     }
 }
