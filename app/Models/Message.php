9450896ee36005447249c77edfbccebda4eb66e1
Sean Shih <tsubasarcs@gmail.com>
2021-08-16 02:18:33 +0800

    install laravel model cacheable

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,6 +12,7 @@ class Message extends Model
 {
     use HasFactory;
     use Searchable;
+    use Cachable;
 
     /**
      * The attributes that are mass assignable.
