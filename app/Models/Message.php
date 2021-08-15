f77c0f83e54060031a3ab9f0da21ea9c9e6bf04b
Sean Shih <tsubasarcs@gmail.com>
2021-08-16 06:58:47 +0800

    adjust

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,12 +6,10 @@ use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Laravel\Scout\Searchable;
 
 class Message extends Model
 {
     use HasFactory;
-    use Searchable;
     use Cachable;
 
     /**
